d7718b3a37f180a5faa5ce06bd971f88a97f6e8c
Shreyash Kalaskar <shreyashrajiokalaskar@gmail.com>
2023-03-15 22:35:07 +0530

    formatted files

--- libs/shared/src/lib/services/onHover-menu.service.ts
+++ libs/shared/src/lib/services/onHover-menu.service.ts
@@ -1,64 +1,62 @@
-import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
+export class HoverMenuService {
+  login = [
+    {
+      menuItem: 'New Customer ?',
+      menuSymbol: '',
+    },
+    {
+      menuItem: 'My Profile',
+      menuSymbol: 'fa fa-user-circle',
+    },
+    {
+      menuItem: 'Flipkart Plus Zone',
+      menuSymbol: 'fa fa-plus',
+    },
+    {
+      menuItem: 'Orders',
+      menuSymbol: 'fa fa-shopping-basket',
+    },
+    {
+      menuItem: 'Wishlist',
+      menuSymbol: 'fa fa-heart',
+    },
+    {
+      menuItem: 'Rewards',
+      menuSymbol: 'fa fa-gift',
+    },
+    {
+      menuItem: 'Giftcards',
+      menuSymbol: 'fa fa-download',
+    },
+  ];
 
-export class HoverMenuService{
+  moreMenuList = [
+    {
+      menuItem: 'Notification Preferences',
+      menuSymbol: 'fa fa-bell',
+    },
+    {
+      menuItem: 'Sell on Flipkart',
+      menuSymbol: 'fa fa-briefcase',
+    },
+    {
+      menuItem: '24X7 Customer Care',
+      menuSymbol: 'fa fa-question-circle',
+    },
+    {
+      menuItem: 'Advertise',
+      menuSymbol: 'fa fa-shopping-basket',
+    },
+    {
+      menuItem: 'Download App',
+      menuSymbol: 'fa fa-download',
+    },
+  ];
 
-    login = [
-                {
-                    menuItem:'New Customer ?',
-                    menuSymbol: ''
-                },
-                {
-                    menuItem: 'My Profile',
-                    menuSymbol: 'fa fa-user-circle'
-                },
-                { 
-                    menuItem: 'Flipkart Plus Zone',
-                    menuSymbol : 'fa fa-plus'
-                },
-                { 
-                    menuItem: 'Orders',
-                    menuSymbol: 'fa fa-shopping-basket'
-                },
-                {
-                    menuItem: 'Wishlist',
-                    menuSymbol: 'fa fa-heart'
-                },
-                {
-                    menuItem: 'Rewards',
-                    menuSymbol: 'fa fa-gift'
-                },
-                {
-                    menuItem: 'Giftcards',
-                    menuSymbol: 'fa fa-download'
-                }
-            ]
-
-    moreMenuList = [
-                {
-                    menuItem: 'Notification Preferences',
-                    menuSymbol: 'fa fa-bell'
-                },
-                {
-                    menuItem: 'Sell on Flipkart',
-                    menuSymbol: 'fa fa-briefcase'
-                },
-                {
-                    menuItem: '24X7 Customer Care',
-                    menuSymbol: 'fa fa-question-circle'
-                },
-                {
-                    menuItem: 'Advertise',
-                    menuSymbol: 'fa fa-shopping-basket'
-                },
-                {
-                    menuItem: 'Download App',
-                    menuSymbol: 'fa fa-download'
-                }
-            ]
-
-    sendMenuList = new Subject();
-    sendPhotoIndex = new Subject();
+  sendMenuList = new Subject();
+  sendPhotoIndex = new Subject();
 }
--- libs/shared/src/lib/services/socket-io.service.ts
+++ libs/shared/src/lib/services/socket-io.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from "@angular/core";
-import { io } from "socket.io-client";
-import { environment } from "../environments/environment";
+import { Injectable } from '@angular/core';
+import { io } from 'socket.io-client';
+import { environment } from '../environments/environment';
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class SocketService {
   socket: any;
 
